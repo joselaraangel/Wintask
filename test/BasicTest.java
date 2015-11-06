81ae9bc9fe79acbe7d3e21c1f0283350ad311bd4
juliob14 <assassinpoint@gmail.com>
2015-11-05 21:40:37 -0700

    test

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,9 +5,21 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
+ @Test
+public void someTestOfASecuredAction() {
+    Map<String, String> loginUserParams = new HashMap<String, String>(); 
+    loginUserParams.put("username", "admin"); 
+    loginUserParams.put("password", "admin");
+
+    // Login here so the following request will be authenticated:
+    Response response = POST("/login", loginUserParams);
+
+    // The following is an action that requires an authenticated user:
+    Map<String, String> params;
+    params.put("someparam", "somevalue");
+    response = POST("/some/secured/action", params);
+
+    assertIsOk(response); // this always fails because it is a 302 redirecting to /login
+}
 
 }
